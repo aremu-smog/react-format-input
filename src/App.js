83e14a2d87ab7ed054add6aa409d4e43cc9941a8
Aremu Smog <aremuoluwagbamila@gmail.com>
2022-04-21 18:35:04 +0100

    feat: form ui and state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import "./App.css"
 
+import React, { useState } from "react"
+
 function App() {
+	const [amount, setAmount] = useState("")
+
+	const handleAmountChange = e => {
+		setAmount(e.target.value)
+	}
+
 	return (
-		<div className='App'>
-			<header className='App-header'></header>
-		</div>
+		<section className='app'>
+			<form className='form'>
+				<h2 className='form__heading'>Make Payment </h2>
+				<div>
+					<input
+						type='text'
+						className='form__input'
+						name='amount'
+						placeholder='Amount'
+						onChange={handleAmountChange}
+						value={amount}
+					/>
+				</div>
+				<div>
+					<input
+						type='submit'
+						className='form__input--submit'
+						value={`Pay now`}
+					/>
+				</div>
+			</form>
+		</section>
 	)
 }
 
